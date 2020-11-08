421f263445be3b7a4717cd1d5297912bd2632974
Paulian Le <43308867+paulian7@users.noreply.github.com>
2020-11-07 19:00:25 -0800

    Button

--- app/src/main/java/com/example/cutiehack2020/MainActivity.kt
+++ app/src/main/java/com/example/cutiehack2020/MainActivity.kt
@@ -4,20 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.core.content.ContextCompat.startActivity
 
 class MainActivity : AppCompatActivity()
 {
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener{
-            val intent = Intent(this, second::class.java)
+            val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
-
     }
 }
