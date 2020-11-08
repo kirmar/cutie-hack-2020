32500c6af2c3646039bc25bce845822699befbe1
Paulian Le <43308867+paulian7@users.noreply.github.com>
2020-11-07 19:37:24 -0800

    Second Button

--- app/src/main/java/com/example/cutiehack2020/MainActivity2.kt
+++ app/src/main/java/com/example/cutiehack2020/MainActivity2.kt
@@ -1,11 +1,19 @@
 package com.example.CutieHack2020
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
